d8b89e5cb03adda4eba9e3de03e1c3096e291777
Danillo <danillo.fr25@gmail.com>
2023-01-25 13:13:00 -0300

    iniciando testes com html

--- dist/extension.js
+++ dist/extension.js
@@ -51,10 +51,33 @@ function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('first-extension.pizza', () => {
         vscode.window.showInformationMessage('Do you want some pizza?', 'yes', 'no');
     }));
+    context.subscriptions.push(vscode.commands.registerCommand('first-extension.first-panel', () => {
+        const panel = vscode.window.createWebviewPanel('first-panel', 'First Panel', vscode.ViewColumn.One, {});
+        panel.webview.html = getWebviewContent();
+    }));
 }
 exports.activate = activate;
 function deactivate() { }
 exports.deactivate = deactivate;
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>First Panel</title>
+  </head>
+	<body>
+		<h1>Base 64 Translater</h1>
+		<form>
+			<textarea name="input-data" placeholder="input ..." rows="30" cols="50"></textarea>
+			<button>Encoding</button>
+			<button>Decoding</button>
+			<textarea name="output-data" placeholder="output ..." rows="30" cols="50"></textarea>
+		</form>
+	</body>
+  </html>`;
+}
 
 })();
 
--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,34 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	);
 
+	context.subscriptions.push(
+		vscode.commands.registerCommand('first-extension.first-panel',() => {
+			const panel = vscode.window.createWebviewPanel('first-panel','First Panel',vscode.ViewColumn.One,{});
+			panel.webview.html = getWebviewContent();
+
+		})
+	);
+
 }
 
 export function deactivate() {}
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>First Panel</title>
+  </head>
+	<body>
+		<h1>Base 64 Translater</h1>
+		<form>
+			<textarea name="input-data" placeholder="input ..." rows="30" cols="50"></textarea>
+			<button>Encoding</button>
+			<button>Decoding</button>
+			<textarea name="output-data" placeholder="output ..." rows="30" cols="50"></textarea>
+		</form>
+	</body>
+  </html>`;
+  }
