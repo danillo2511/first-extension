08bc56de48678e96b669d1c23cf131a4e5af259c
Danillo <danillo.fr25@gmail.com>
2023-01-25 10:58:30 -0300

    ajuste nas configs para rodar

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,18 @@ export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "first-extension" is now active!');
 
-	context.subscriptions.push(vscode.commands.registerCommand('first-extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from first-extension!');}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('first-extension.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from first-extension!');
+		})
+	);
 
+	context.subscriptions.push(
+		vscode.commands.registerCommand('first-extension.pizza', () => {
+			vscode.window.showInformationMessage('Do you want some pizza?','yes','no');
+		})
+	);
 
-	}
+}
 
 export function deactivate() {}
